2f9c8025546c2704746d270c3fcbc54d0cb8ff97
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-10-14 14:54:06 +0700

    Create queue.h

---
+++ include/queue.h
@@ -0,0 +1,22 @@
+#ifndef queue_h
+#define queue_h
+
+#include "utility.h"
+
+struct Queue {
+    string* data;
+    size_t size;
+    int front; // Индекс первого элемента
+    int rear; // Индекс последнего элемента
+    int capacity; // Вместимость очереди
+
+    Queue(int cap);
+    ~Queue();
+
+    bool isempty();
+    void push(string value); //Функция добаления элемента
+    string pop(); // функция удаления элемента с начала очереди и его возврат
+    string peek(); // функция вывода элемента в начале очереди
+};
+
+#endif //  QUEUE_H
