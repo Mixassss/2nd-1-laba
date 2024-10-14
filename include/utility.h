6870ddd4613dceb8158c6890acbba10f0989b8d0
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-10-14 14:49:28 +0700

    Create utility.h

---
+++ include/utility.h
@@ -0,0 +1,18 @@
+#ifndef utility_h
+#define utility_h
+
+#include <iostream>
+#include<iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Data {
+  string name;
+  string str;
+  int numLine;
+};
+
+#endif // UTILITY_H 
