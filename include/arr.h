b09d59c86385b16f6cb9c6a3c54ae7d81940314d
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-10-14 14:51:50 +0700

    Create arr.h

---
+++ include/arr.h
@@ -0,0 +1,25 @@
+#ifndef _arr_h
+#define _arr_h
+
+#include "utility.h"
+
+struct Array {
+    string* arr;
+    size_t size; //Размер массива
+    size_t razmer;
+     
+    Array(); //Конструктор
+    ~Array(); //Деструктор
+
+    void ShowArray() const; // Вывод массива
+    void addToEnd(string value); //Добавление элемента в конец массива
+    void addAtIndex(size_t index, string value); // Добавление по индексу
+    string getIndex(size_t index); //Получение элемента по индексу
+    void removeAtIndex(size_t index); //Удаление элемента по индексу
+    void replaceAtIndex(size_t index, string value);  // Замена элемента по индексу
+    size_t getSize() const;
+};
+
+#include "../src/array.cpp"
+
+#endif // ARR_H
