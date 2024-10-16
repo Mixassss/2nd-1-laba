c5603e63ab6a22747c4664e8000eb64506ecb89c
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-10-16 21:28:35 +0700

    Изменил меню заголовочный

--- include/menu.h
+++ include/menu.h
@@ -10,15 +10,13 @@
 
 #include "utility.h"
 
-void aMenu(Node *tokens, Array &data);
-void lsMenu(Node* tokens, Data &data);
-void ldMenu(Node* tokens, Data &data);
-void qMenu(Node *tokens, Data &data);
-void sMenu(Node* tokens, Data& data);
-void hMenu(Node *tokens, Data &data);
-void tMenu(Node *tokens, Data &data);
-Node* parse(const string &input);
-void addToData(Data &data, NodeT *node);
-size_t stringToSize(const string &str);
+void write(const string& path, const string& text);
+string Ftext(const string& path, const string& nameStruct);
+void aMenu(const string& command, const string& path);
+void lMenu(const string& command, const string& path);
+void qMenu(const string& command, const string& path);
+void sMenu(string& command, string& path);
+void hMenu(string& command, string& path);
+void tMenu(string& command, string& path);
 
 #endif // MENU_H
